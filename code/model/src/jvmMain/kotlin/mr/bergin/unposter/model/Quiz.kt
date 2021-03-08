200cdee06ca8b14ff295c09fb46acfafbe43e695
Jordan Bergin <jordan.j.bergin@protonmail.com>
2021-03-08 20:55:07 +0000

    Adding in concept of an in progress quiz.

--- code/model/src/jvmMain/kotlin/mr/bergin/unposter/model/Quiz.kt
+++ code/model/src/jvmMain/kotlin/mr/bergin/unposter/model/Quiz.kt
@@ -5,9 +5,38 @@ class Quiz(
     val questionPool: List<Question<*>>,
 )
 
-fun Quiz.complete(answers: Set<AnsweredQuestion<*>>) = CompletedQuiz(this, answers)
+fun Quiz.start(user: User, limit: Int) = InProgressQuiz(user, this, limit)
+
+sealed class UserQuiz
+
+class InProgressQuiz(
+    val user: User,
+    val quiz: Quiz,
+    val limit: Int,
+    val currentQuestion: Question<*> = quiz.questionPool.random(),
+    val answeredQuestions: List<AnsweredQuestion<*>> = listOf(),
+) : UserQuiz()
 
 class CompletedQuiz(
+    val user: User,
     val quiz: Quiz,
-    val answers: Set<AnsweredQuestion<*>>,
-)
+    val limit: Int,
+    val answeredQuestions: List<AnsweredQuestion<*>> = listOf(),
+) : UserQuiz()
+
+fun InProgressQuiz.answerQuestion(
+    answeredQuestion: AnsweredQuestion<*>,
+): UserQuiz {
+    val newAnsweredQuestions = answeredQuestions + answeredQuestion
+    return if (newAnsweredQuestions.size == limit) {
+        CompletedQuiz(user, quiz, limit, newAnsweredQuestions)
+    } else {
+        InProgressQuiz(
+            user,
+            quiz,
+            limit,
+            quiz.questionPool.random(),
+            newAnsweredQuestions
+            )
+    }
+}
